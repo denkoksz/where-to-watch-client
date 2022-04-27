005b9380c51424fe998b0f5495251ec6cef38661
denkoksz <denkoksz@gmail.com>
2022-04-27 21:11:28 +0200

    add navbar for site

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar/>
+  <div :style="{ 'margin-left': sidebarWidth }">
+    <router-view/>
+  </div>
 </template>
+
+<script>
+import Navbar from "@/components/navbar/Navbar";
+import { sidebarWidth } from "@/components/navbar/state";
+
+export default {
+  components: { Navbar },
+  setup() {
+    return { sidebarWidth }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +24,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="sidebar" :style="{ width: sidebarWidth }">
+    <h1>
+      <span v-if="collapsed">
+        <div>H</div>
+        <div>N</div>
+      </span>
+      <span v-else>Hol nézzem?</span>
+    </h1>
+
+    <NavbarLink to="/" icon="fas fa-home">Kezdőlap</NavbarLink>
+    <NavbarLink to="/sportevents" icon="fas fa-event">Események</NavbarLink>
+    <NavbarLink to="/bars" icon="fas fa-bar">Helyszínek</NavbarLink>
+
+    <span
+        class="collapse-icon"
+        :class="{ 'rotate-180': collapsed }"
+        @click="toggleSidebar"
+    >
+      <p class="toggle">>></p>
+    </span>
+  </div>
+</template>
+
+<script>
+import {collapsed, toggleSidebar, sidebarWidth} from './state';
+import NavbarLink from "@/components/navbar/NavbarLink";
+
+export default {
+  components: {NavbarLink},
+  setup() {
+      return { collapsed, toggleSidebar, sidebarWidth };
+  }
+}
+</script>
+
+<style>
+  :root {
+    --sidebar-bg-color: #2f855a;
+    --sidebar-item-hover: #38a169;
+    --sidebar-item-active: #276749;
+  }
+</style>
+
+<style scoped>
+  .sidebar {
+    color: white;
+    background-color: var(--sidebar-bg-color);
+
+    float: left;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0.5em;
+
+    transition: 0.3s ease;
+
+    display: flex;
+    flex-direction: column;
+  }
+  .collapse-icon {
+    position: absolute;
+    bottom: 0;
+    padding: 0.75em;
+
+    color: rgba(255, 255, 255, 0.7);
+
+    transition: 0.2s linear;
+  }
+  .rotate-180 {
+    transform: rotate(180deg);
+    transition: 0.2s linear;
+  }
+  .toggle:hover {
+    cursor: pointer;
+  }
+</style>
